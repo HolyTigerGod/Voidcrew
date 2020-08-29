HolyTigerGod/Voidcrew

commit f7b26ec757ac9a455535023de6b4c5889f08a412

Bools and returns super-pr (#53221)
Replaces like 70-80% of 0 and such, as a side effect cleaned up a bunch of returns
Edit: Most left out ones are in mecha which should be done in mecha refactor already
Oh my look how clean it is

Co-authored-by: TiviPlus <TiviPlus>
Co-authored-by: Couls <coul422@gmail.com>

code/modules/mob/living/silicon/silicon_say.dm
@@ -24,15 +24,15 @@
 			to_chat(M, "<span class='binarysay'>[link] [rendered]</span>")
 
 /mob/living/silicon/binarycheck()
-	return 1
+	return TRUE
 
 /mob/living/silicon/lingcheck()
-	return 0 //Borged or AI'd lings can't speak on the ling channel.
+	return FALSE //Borged or AI'd lings can't speak on the ling channel.
 
 /mob/living/silicon/radio(message, list/message_mods = list(), list/spans, language)
 	. = ..()
-	if(. != 0)
-		return .
+	if(!.)
+		return
 	if(message_mods[MODE_HEADSET])
 		if(radio)
 			radio.talk_into(src, message, , spans, language, message_mods)
@@ -42,4 +42,4 @@
 			radio.talk_into(src, message, message_mods[RADIO_EXTENSION], spans, language, message_mods)
 			return ITALICS | REDUCE_RANGE
 
-	return 0
+	return FALSE

code/modules/mob/living/simple_animal/bot/bot.dm
@@ -404,7 +404,7 @@
 
 /mob/living/simple_animal/bot/radio(message, list/message_mods = list(), list/spans, language)
 	. = ..()
-	if(. != 0)
+	if(!.)
 		return
 
 	if(message_mods[MODE_HEADSET])
@@ -693,7 +693,7 @@ Pass a positive integer as an argument to override a bot's default speed.
 		patrol_target = nearest_beacon_loc
 		destination = next_destination
 	else
-		auto_patrol = 0
+		auto_patrol = FALSE
 		mode = BOT_IDLE
 		speak("Disengaging patrol mode.")
 
@@ -732,11 +732,11 @@ Pass a positive integer as an argument to override a bot's default speed.
 	// process control input
 	switch(command)
 		if("patroloff")
-			bot_reset() //HOLD IT!!
-			auto_patrol = 0
+			bot_reset() //HOLD IT!!	//OBJECTION!!
+			auto_patrol = FALSE
 
 		if("patrolon")
-			auto_patrol = 1
+			auto_patrol = TRUE
 
 		if("summon")
 			bot_reset()
@@ -751,7 +751,7 @@ Pass a positive integer as an argument to override a bot's default speed.
 			ejectpairemote(user)
 	return
 
-//
+
 /mob/living/simple_animal/bot/proc/bot_control_message(command, user)
 	switch(command)
 		if("patroloff")
@@ -831,7 +831,7 @@ Pass a positive integer as an argument to override a bot's default speed.
 	dat = get_controls(M)
 	var/datum/browser/popup = new(M,window_id,window_name,350,600)
 	popup.set_content(dat)
-	popup.open(use_onclose = 0)
+	popup.open(use_onclose = FALSE)
 	onclose(M,window_id,ref=src)
 	return
 

code/modules/mob/living/simple_animal/parrot.dm
@@ -159,8 +159,8 @@
 
 /mob/living/simple_animal/parrot/radio(message, list/message_mods = list(), list/spans, language) //literally copied from human/radio(), but there's no other way to do this. at least it's better than it used to be.
 	. = ..()
-	if(. != 0)
-		return .
+	if(!.)
+		return
 
 	if(message_mods[MODE_HEADSET])
 		if(ears)
@@ -175,7 +175,7 @@
 			ears.talk_into(src, message, message_mods[RADIO_EXTENSION], spans, language, message_mods)
 			return ITALICS | REDUCE_RANGE
 
-	return 0
+	return FALSE
 
 /*
  * Inventory
@@ -605,12 +605,12 @@
 				parrot_state = PARROT_WANDER
 				parrot_stuck = 0
 				parrot_lastmove = null
-				return 1
+				return TRUE
 		else
 			parrot_lastmove = null
 	else
 		parrot_lastmove = src.loc
-	return 0
+	return FALSE
 
 /mob/living/simple_animal/parrot/proc/search_for_item()
 	var/item
@@ -633,8 +633,6 @@
 				item = null
 				continue
 			return item
-
-	return null
 
 /mob/living/simple_animal/parrot/proc/search_for_perch()
 	for(var/obj/O in view(src))

commit 3dcd6b6e66b9d845c2cae345d7493560ab418dac

Fixes silicon/parrot radio (#53296)
Silicons and parrots can speak into their radios again.
This was broken in #53221 because the . != 0 was incorrectly translated to !.

code/modules/mob/living/silicon/silicon_say.dm
@@ -31,7 +31,7 @@
 
 /mob/living/silicon/radio(message, list/message_mods = list(), list/spans, language)
 	. = ..()
-	if(!.)
+	if(.)
 		return
 	if(message_mods[MODE_HEADSET])
 		if(radio)

code/modules/mob/living/simple_animal/bot/bot.dm
@@ -404,7 +404,7 @@
 
 /mob/living/simple_animal/bot/radio(message, list/message_mods = list(), list/spans, language)
 	. = ..()
-	if(!.)
+	if(.)
 		return
 
 	if(message_mods[MODE_HEADSET])

code/modules/mob/living/simple_animal/parrot.dm
@@ -159,7 +159,7 @@
 
 /mob/living/simple_animal/parrot/radio(message, list/message_mods = list(), list/spans, language) //literally copied from human/radio(), but there's no other way to do this. at least it's better than it used to be.
 	. = ..()
-	if(!.)
+	if(.)
 		return
 
 	if(message_mods[MODE_HEADSET])