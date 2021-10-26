HolyTigerGod/Voidcrew

commit ab3fc8e2b9de247a8fffce02ee575bc8c29f8a11

Adds Neon Carpet (#59140)
Adds a couple varieties of neon carpet.
Makes decals care about their plane in addition to their layer.

code/game/turfs/closed/wall/reinf_walls.dm
@@ -197,9 +197,9 @@
 				return TRUE
 	return FALSE
 
-/turf/closed/wall/r_wall/update_icon()
+/turf/closed/wall/r_wall/update_icon(updates=ALL)
 	. = ..()
-	if(d_state != INTACT)
+	if(!(updates & UPDATE_SMOOTHING) || (d_state != INTACT))
 		smoothing_flags = NONE
 		return
 	smoothing_flags = SMOOTH_BITMASK

commit 4e93e1a4f8ac998bffced2475622ce446ebe3e55

Fix RWall smoothing (#62327)
* Fix rwall smoothing

This was broken in ab3fc8e2b9. R-walls would update with ~UPDATE_SMOOTHING
after smoothing was complete, causing their smoothing_flags to become NONE.

This effectively meant that all R-walls would smooth exactly once, and any
further updates, such as building a new R-wall next to an existing one,
would fail to update the existing wall.

By keeping distinct the case where the R-wall is no longer intact, and
so should no longer cause smoothing, and the case where no UPDATE_SMOOTHING
is provided so the update should early return, we can avoid overwriting
smoothing_flags unnecessarily.

* RWall: Smooth Neighbours when being destroyed too

code/game/turfs/closed/wall/reinf_walls.dm
@@ -200,8 +200,10 @@
 
 /turf/closed/wall/r_wall/update_icon(updates=ALL)
 	. = ..()
-	if(!(updates & UPDATE_SMOOTHING) || (d_state != INTACT))
+	if(d_state != INTACT)
 		smoothing_flags = NONE
+		return
+	if (!(updates & UPDATE_SMOOTHING))
 		return
 	smoothing_flags = SMOOTH_BITMASK
 	QUEUE_SMOOTH_NEIGHBORS(src)

code/game/turfs/closed/walls.dm
@@ -87,6 +87,7 @@
 		ChangeTurf(decon_type, flags = CHANGETURF_INHERIT_AIR)
 	else
 		ScrapeAway()
+	QUEUE_SMOOTH_NEIGHBORS(src)
 
 /turf/closed/wall/proc/break_wall()
 	new sheet_type(src, sheet_amount)