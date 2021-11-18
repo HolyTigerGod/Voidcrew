HolyTigerGod/Voidcrew

commit 4e5458011e8751faaa186201f34197ae44cb58a0

Fixes loading funny admin nicknames   (#62881)

code/__HELPERS/_string_lists.dm
@@ -19,19 +19,24 @@ GLOBAL_VAR(string_filename_current_key)
 	else
 		CRASH("strings list not found: [STRING_DIRECTORY]/[filepath], index=[key]")
 
-/proc/strings(filepath as text, key as text)
+/proc/strings(filepath as text, key as text, directory = STRING_DIRECTORY)
+	if(IsAdminAdvancedProcCall())
+		return
+
 	filepath = sanitize_filepath(filepath)
-	load_strings_file(filepath)
+	load_strings_file(filepath, directory)
 	if((filepath in GLOB.string_cache) && (key in GLOB.string_cache[filepath]))
 		return GLOB.string_cache[filepath][key]
 	else
-		CRASH("strings list not found: [STRING_DIRECTORY]/[filepath], index=[key]")
+		CRASH("strings list not found: [directory]/[filepath], index=[key]")
 
 /proc/strings_subkey_lookup(match, group1)
 	return pick_list(GLOB.string_filename_current_key, group1)
 
-/proc/load_strings_file(filepath)
-	filepath = sanitize_filepath(filepath) // in case we're called directly
+/proc/load_strings_file(filepath, directory)
+	if(IsAdminAdvancedProcCall())
+		return
+
 	GLOB.string_filename_current_key = filepath
 	if(filepath in GLOB.string_cache)
 		return //no work to do
@@ -39,7 +44,7 @@ GLOBAL_VAR(string_filename_current_key)
 	if(!GLOB.string_cache)
 		GLOB.string_cache = new
 
-	if(fexists("[STRING_DIRECTORY]/[filepath]"))
-		GLOB.string_cache[filepath] = json_load("[STRING_DIRECTORY]/[filepath]")
+	if(fexists("[directory]/[filepath]"))
+		GLOB.string_cache[filepath] = json_load("[directory]/[filepath]")
 	else
-		CRASH("file not found: [STRING_DIRECTORY]/[filepath]")
+		CRASH("file not found: [directory]/[filepath]")

commit 076c7ce217bd723b5e009a107be21bf6eceaf6f8

Fixes loading scars file (#62928)
4e5458011e8751faaa186201f34197ae44cb58a0 broke loading default string files with load_strings_file(), since there was no default directory

code/__HELPERS/_string_lists.dm
@@ -33,7 +33,7 @@ GLOBAL_VAR(string_filename_current_key)
 /proc/strings_subkey_lookup(match, group1)
 	return pick_list(GLOB.string_filename_current_key, group1)
 
-/proc/load_strings_file(filepath, directory)
+/proc/load_strings_file(filepath, directory = STRING_DIRECTORY)
 	if(IsAdminAdvancedProcCall())
 		return
 