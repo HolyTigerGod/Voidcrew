HolyTigerGod/Voidcrew

commit 04d1cba66297b180129303ff278373cf86b33191

Cleanbot fixes and adjustments (#71159)
## About The Pull Request

Fixed cleanbots not prioritizing adjacent tiles.
Made them able to clean 1 tile away (they have that arm, just like
medibots), enabling them cleaning under the tables.
Expanded the list of cleanable objects for cleanbots with missing
dirt/trash types.

Cleanbots had non-working code that made them clean very inefficiently,
running around, instead of going 1 by 1 tile next to them. Now they do a
bit better job at this and also don't leave the mess under the
tables/machinery.

code/modules/mob/living/simple_animal/bot/cleanbot.dm
@@ -285,29 +285,25 @@
 			mode = BOT_IDLE
 			return
 
-		if(loc == get_turf(target))
-			if(check_bot(target))
-				shuffle = TRUE //Shuffle the list the next time we scan so we dont both go the same way.
-				path = list()
-			else
-				UnarmedAttack(target, proximity_flag = TRUE) //Rather than check at every step of the way, let's check before we do an action, so we can rescan before the other bot.
-				if(QDELETED(target)) //We done here.
-					target = null
-					mode = BOT_IDLE
-					return
+		if(get_dist(src, target) <= 1)
+			UnarmedAttack(target, proximity_flag = TRUE) //Rather than check at every step of the way, let's check before we do an action, so we can rescan before the other bot.
+			if(QDELETED(target)) //We done here.
+				target = null
+				mode = BOT_IDLE
+				return
 
-		if(!length(path)) //No path, need a new one
-			//Try to produce a path to the target, and ignore airlocks to which it has access.
-			path = get_path_to(src, target, 30, id=access_card)
-			if(!bot_move(target))
+		if(target && path.len == 0 && (get_dist(src,target) > 1))
+			path = get_path_to(src, target, max_distance=30, mintargetdist=1, id=access_card)
+			mode = BOT_MOVING
+			if(!path.len)
 				add_to_ignore(target)
 				target = null
 				path = list()
-				return
-			mode = BOT_MOVING
-		else if(!bot_move(target))
-			target = null
-			mode = BOT_IDLE
+
+		if(path.len > 0 && target)
+			if(!bot_move(path[path.len]))
+				target = null
+				mode = BOT_IDLE
 			return
 
 /mob/living/simple_animal/bot/cleanbot/proc/get_targets()
@@ -318,6 +314,7 @@
 	//main targets
 	target_types = list(
 		/obj/effect/decal/cleanable/oil,
+		/obj/effect/decal/cleanable/fuel_pool,
 		/obj/effect/decal/cleanable/vomit,
 		/obj/effect/decal/cleanable/robot_debris,
 		/obj/effect/decal/cleanable/molten_object,
@@ -326,6 +323,12 @@
 		/obj/effect/decal/cleanable/greenglow,
 		/obj/effect/decal/cleanable/dirt,
 		/obj/effect/decal/cleanable/insectguts,
+		/obj/effect/decal/cleanable/generic,
+		/obj/effect/decal/cleanable/shreds,
+		/obj/effect/decal/cleanable/glass,
+		/obj/effect/decal/cleanable/wrapping,
+		/obj/effect/decal/cleanable/glitter,
+		/obj/effect/decal/cleanable/confetti,
 		/obj/effect/decal/remains,
 	)
 
@@ -340,6 +343,7 @@
 		target_types += list(
 			/mob/living/basic/cockroach,
 			/mob/living/basic/mouse,
+			/obj/effect/decal/cleanable/ants,
 		)
 
 	if(janitor_mode_flags & CLEANBOT_CLEAN_DRAWINGS)

code/modules/mob/living/simple_animal/bot/bot.dm
@@ -495,14 +495,7 @@
  * scan_range - how far away from [src] will be scanned, if nothing is found directly adjacent.
  */
 /mob/living/simple_animal/bot/proc/scan(list/scan_types, old_target, scan_range = DEFAULT_SCAN_RANGE)
-	var/turf/current_turf = get_turf(src)
-	if(!current_turf)
-		return
-	var/list/adjacent = current_turf.get_atmos_adjacent_turfs(1)
-	if(shuffle) //If we were on the same tile as another bot, let's randomize our choices so we dont both go the same way
-		adjacent = shuffle(adjacent)
-		shuffle = FALSE
-
+	var/list/adjacent = shuffle(view(1, src))
 	for(var/turf/scan as anything in adjacent) //Let's see if there's something right next to us first!
 		if(check_bot(scan)) //Is there another bot there? Then let's just skip it
 			continue
@@ -510,7 +503,7 @@
 		if(final_result)
 			return final_result
 
-	for(var/turf/scanned_turfs as anything in shuffle(view(scan_range, src)) - adjacent) //Search for something in range, minus what we already checked.
+	for(var/turf/scanned_turfs as anything in view(scan_range, src) - adjacent) //Search for something in range, minus what we already checked.
 		if(check_bot(scanned_turfs)) //Is there another bot there? Then let's just skip it
 			continue
 		var/final_result = checkscan(scanned_turfs, scan_types, old_target)
@@ -600,7 +593,7 @@ Pass a positive integer as an argument to override a bot's default speed.
 
 	//For giving the bot temporary all-access. This method is bad and makes me feel bad. Refactoring access to a component is for another PR.
 	var/obj/item/card/id/all_access = new /obj/item/card/id/advanced/gold/captains_spare()
-	set_path(get_path_to(src, waypoint, 200, id = all_access))
+	set_path(get_path_to(src, waypoint, max_distance=200, id = all_access))
 	qdel(all_access)
 	calling_ai = caller //Link the AI to the bot!
 	ai_waypoint = waypoint
@@ -816,14 +809,14 @@ Pass a positive integer as an argument to override a bot's default speed.
 // given an optional turf to avoid
 /mob/living/simple_animal/bot/proc/calc_path(turf/avoid)
 	check_bot_access()
-	set_path(get_path_to(src, patrol_target, 120, id=access_card, exclude=avoid))
+	set_path(get_path_to(src, patrol_target, max_distance=120, id=access_card, exclude=avoid))
 
 /mob/living/simple_animal/bot/proc/calc_summon_path(turf/avoid)
 	check_bot_access()
 	INVOKE_ASYNC(src, .proc/do_calc_summon_path, avoid)
 
 /mob/living/simple_animal/bot/proc/do_calc_summon_path(turf/avoid)
-	set_path(get_path_to(src, summon_target, 150, id=access_card, exclude=avoid))
+	set_path(get_path_to(src, summon_target, max_distance=150, id=access_card, exclude=avoid))
 	if(!length(path)) //Cannot reach target. Give up and announce the issue.
 		speak("Summon command failed, destination unreachable.",radio_channel)
 		bot_reset()

code/modules/mob/living/simple_animal/bot/firebot.dm
@@ -224,7 +224,7 @@
 
 	if(target_fire && (get_dist(src, target_fire) > 2))
 
-		path = get_path_to(src, target_fire, 30, 1, id=access_card)
+		path = get_path_to(src, target_fire, max_distance=30, mintargetdist=1, id=access_card)
 		mode = BOT_MOVING
 		if(!path.len)
 			soft_reset()

code/modules/mob/living/simple_animal/bot/floorbot.dm
@@ -248,9 +248,9 @@
 		if(!length(path))
 			if(!isturf(target))
 				var/turf/TL = get_turf(target)
-				path = get_path_to(src, TL, 30, id=access_card,simulated_only = FALSE)
+				path = get_path_to(src, TL, max_distance=30, id=access_card,simulated_only = FALSE)
 			else
-				path = get_path_to(src, target, 30, id=access_card,simulated_only = FALSE)
+				path = get_path_to(src, target, max_distance=30, id=access_card,simulated_only = FALSE)
 
 			if(!bot_move(target))
 				add_to_ignore(target)

code/modules/mob/living/simple_animal/bot/medbot.dm
@@ -392,10 +392,10 @@
 		return
 
 	if(patient && path.len == 0 && (get_dist(src,patient) > 1))
-		path = get_path_to(src, patient, 30,id=access_card)
+		path = get_path_to(src, patient, max_distance=30, id=access_card)
 		mode = BOT_MOVING
 		if(!path.len) //try to get closer if you can't reach the patient directly
-			path = get_path_to(src, patient, 30,1,id=access_card)
+			path = get_path_to(src, patient, max_distance=30, mintargetdist=1, id=access_card)
 			if(!path.len) //Do not chase a patient we cannot reach.
 				soft_reset()
 

code/modules/mob/living/simple_animal/bot/mulebot.dm
@@ -586,7 +586,7 @@
 // calculates a path to the current destination
 // given an optional turf to avoid
 /mob/living/simple_animal/bot/mulebot/calc_path(turf/avoid = null)
-	path = get_path_to(src, target, 250, id=access_card, exclude=avoid)
+	path = get_path_to(src, target, max_distance=250, id=access_card, exclude=avoid)
 
 // sets the current destination
 // signals all beacons matching the delivery code

commit 02834ce313bb8c9c8d97758b92723459765f155b

Fix cleanbots spamming runtimes (#71583)
Broken in #71159. `get_path_to` can return null.

code/modules/mob/living/simple_animal/bot/cleanbot.dm
@@ -295,7 +295,7 @@
 		if(target && path.len == 0 && (get_dist(src,target) > 1))
 			path = get_path_to(src, target, max_distance=30, mintargetdist=1, id=access_card)
 			mode = BOT_MOVING
-			if(!path.len)
+			if(length(path) == 0)
 				add_to_ignore(target)
 				target = null
 				path = list()