HolyTigerGod/Voidcrew

commit 391fa83756673a9debe15ec15415bf91cbe9fe80

Quirks are passed an incoming client when applied, allowing quirks to read preferences in `add` and `add_unique`. Renders visual quirks on the preference menu dummy. (#72158)
## About The Pull Request

Closes #72141

Roundstart humans weren't in control of their mob by the time
`AssignQuirks` was called.
The call chain for roundstart goes `create_characters` ->
`equip_characters` -> `AssignQuirks` -> `transfer_characters`.
For latejoin, `create_character` -> `transfer_character` ->
`AssignQuirks`.

I could simply move around the call chain, but that feels much more
fragile and more liable to cause other issues to me. So instead, I
simply allowed add quirk to be passed a client, so that a client's
quirks can be applied to a mob they are not currently inhabiting.

In doing this, it became possible to show visual quirks on the prefs
dummy, like nearsighted glasses and heterochromia. So I put in a little
work to accomplish that as well.


![image](https://user-images.githubusercontent.com/51863163/209030560-58396d43-6ac2-40c5-b13f-1178dc8962fd.png)

Along side, some refactoring and documentation for quirk datums. 

## Why It's Good For The Game

People get what they expect on roundstart. 

## Changelog

:cl: Melbert
qol: The preview dummy in the preferences menu now shows some visual
quirks, like Heterochromia or nearsighted.
fix: Fixed some quirks which read a preference roundstart not applying
the preference.
refactor: Refactored some bits of quirk datums and the quirk application
process.
/:cl:

Co-authored-by: Time-Green <timkoster1@hotmail.com>

code/datums/quirks/neutral_quirks.dm
@@ -44,7 +44,7 @@
 	medical_record_text = "Patient does not speak Galactic Common and may require an interpreter."
 	mail_goodies = list(/obj/item/taperecorder) // for translation
 
-/datum/quirk/foreigner/add()
+/datum/quirk/foreigner/add(client/client_source)
 	var/mob/living/carbon/human/human_holder = quirk_holder
 	human_holder.add_blocked_language(/datum/language/common)
 	if(ishumanbasic(human_holder))
@@ -66,7 +66,7 @@
 	medical_record_text = "Patient reports a vegetarian diet."
 	mail_goodies = list(/obj/effect/spawner/random/food_or_drink/salad)
 
-/datum/quirk/vegetarian/add()
+/datum/quirk/vegetarian/add(client/client_source)
 	var/mob/living/carbon/human/human_holder = quirk_holder
 	var/datum/species/species = human_holder.dna.species
 	species.liked_food &= ~MEAT
@@ -109,7 +109,7 @@
 	medical_record_text = "Patient demonstrates a pathological love of pineapple."
 	mail_goodies = list(/obj/item/food/pizzaslice/pineapple)
 
-/datum/quirk/pineapple_liker/add()
+/datum/quirk/pineapple_liker/add(client/client_source)
 	var/mob/living/carbon/human/human_holder = quirk_holder
 	var/datum/species/species = human_holder.dna.species
 	species.liked_food |= PINEAPPLE
@@ -141,7 +141,7 @@
 		/obj/item/food/pizzaslice/sassysage,
 	)
 
-/datum/quirk/pineapple_hater/add()
+/datum/quirk/pineapple_hater/add(client/client_source)
 	var/mob/living/carbon/human/human_holder = quirk_holder
 	var/datum/species/species = human_holder.dna.species
 	species.disliked_food |= PINEAPPLE
@@ -167,7 +167,7 @@
 	medical_record_text = "Patient demonstrates irregular nutrition preferences."
 	mail_goodies = list(/obj/item/food/urinalcake, /obj/item/food/badrecipe) // Mhhhmmm yummy
 
-/datum/quirk/deviant_tastes/add()
+/datum/quirk/deviant_tastes/add(client/client_source)
 	var/mob/living/carbon/human/human_holder = quirk_holder
 	var/datum/species/species = human_holder.dna.species
 	var/liked = species.liked_food
@@ -192,36 +192,27 @@
 	name = "Heterochromatic"
 	desc = "One of your eyes is a different color than the other!"
 	icon = "eye-low-vision" // Ignore the icon name, its actually a fairly good representation of different color eyes
+	quirk_flags = QUIRK_HUMAN_ONLY|QUIRK_CHANGES_APPEARANCE
 	value = 0
 	mail_goodies = list(/obj/item/clothing/glasses/eyepatch)
-	var/color
 
-/datum/quirk/heterochromatic/add()
-	color = color || quirk_holder.client?.prefs?.read_preference(/datum/preference/color/heterochromatic)
+// Only your first eyes are heterochromatic
+// If someone comes and says "well mr coder you can have DNA bound heterochromia so it's not unrealistic
+// to allow all inserted replacement eyes to become heterochromatic or for it to transfer between mobs"
+// Then just change this to [proc/add] I really don't care
+/datum/quirk/heterochromatic/add_unique(client/client_source)
+	var/color = client_source?.prefs.read_preference(/datum/preference/color/heterochromatic)
 	if(!color)
 		return
 
-	link_to_holder()
+	apply_heterochromatic_eyes(color)
 
-/datum/quirk/heterochromatic/post_add()
-	if(color)
-		return
-
-	color = quirk_holder.client?.prefs?.read_preference(/datum/preference/color/heterochromatic)
-	if(!color)
-		return
-
-	link_to_holder()
-
-/datum/quirk/heterochromatic/remove()
-	UnregisterSignal(quirk_holder, COMSIG_CARBON_LOSE_ORGAN)
-
-/datum/quirk/heterochromatic/proc/link_to_holder()
+/// Applies the passed color to this mob's eyes
+/datum/quirk/heterochromatic/proc/apply_heterochromatic_eyes(color)
 	var/mob/living/carbon/human/human_holder = quirk_holder
+	var/was_not_hetero = !human_holder.eye_color_heterochromatic
 	human_holder.eye_color_heterochromatic = TRUE
 	human_holder.eye_color_right = color
-	// We set override to TRUE as link to holder will be called whenever the preference is applied, given this quirk exists on the mob
-	RegisterSignal(human_holder, COMSIG_CARBON_LOSE_ORGAN, PROC_REF(check_eye_removal), override=TRUE)
 
 	var/obj/item/organ/internal/eyes/eyes_of_the_holder = quirk_holder.getorgan(/obj/item/organ/internal/eyes)
 	if(!eyes_of_the_holder)
@@ -230,6 +221,15 @@
 	eyes_of_the_holder.eye_color_right = color
 	eyes_of_the_holder.old_eye_color_right = color
 	eyes_of_the_holder.refresh()
+
+	if(was_not_hetero)
+		RegisterSignal(human_holder, COMSIG_CARBON_LOSE_ORGAN, PROC_REF(check_eye_removal))
+
+/datum/quirk/heterochromatic/remove()
+	var/mob/living/carbon/human/human_holder = quirk_holder
+	human_holder.eye_color_heterochromatic = FALSE
+	human_holder.eye_color_right = human_holder.eye_color_left
+	UnregisterSignal(human_holder, COMSIG_CARBON_LOSE_ORGAN)
 
 /datum/quirk/heterochromatic/proc/check_eye_removal(datum/source, obj/item/organ/internal/eyes/removed)
 	SIGNAL_HANDLER
@@ -240,7 +240,7 @@
 	// Eyes were removed, remove heterochromia from the human holder and bid them adieu
 	var/mob/living/carbon/human/human_holder = quirk_holder
 	human_holder.eye_color_heterochromatic = FALSE
-	human_holder.eye_color_right = initial(human_holder.eye_color_right)
+	human_holder.eye_color_right = human_holder.eye_color_left
 	UnregisterSignal(human_holder, COMSIG_CARBON_LOSE_ORGAN)
 
 /datum/quirk/monochromatic
@@ -256,7 +256,7 @@
 		/obj/item/clothing/head/fedora/white,
 	)
 
-/datum/quirk/monochromatic/add()
+/datum/quirk/monochromatic/add(client/client_source)
 	quirk_holder.add_client_colour(/datum/client_colour/monochrome)
 
 /datum/quirk/monochromatic/post_add()
@@ -273,21 +273,16 @@
 	icon = "spider"
 	value = 0
 	medical_record_text = "Patient has an irrational fear of something."
-	var/phobia
 	mail_goodies = list(/obj/item/clothing/glasses/blindfold, /obj/item/storage/pill_bottle/psicodine)
 
-/datum/quirk/phobia/add()
-	phobia = phobia || quirk_holder.client?.prefs?.read_preference(/datum/preference/choiced/phobia)
-
-	if(phobia)
-		var/mob/living/carbon/human/human_holder = quirk_holder
-		human_holder.gain_trauma(new /datum/brain_trauma/mild/phobia(phobia), TRAUMA_RESILIENCE_ABSOLUTE)
-
-/datum/quirk/phobia/post_add()
+// Phobia will follow you between transfers
+/datum/quirk/phobia/add(client/client_source)
+	var/phobia = client_source?.prefs.read_preference(/datum/preference/choiced/phobia)
 	if(!phobia)
-		var/mob/living/carbon/human/human_holder = quirk_holder
-		phobia = human_holder.client.prefs.read_preference(/datum/preference/choiced/phobia)
-		human_holder.gain_trauma(new /datum/brain_trauma/mild/phobia(phobia), TRAUMA_RESILIENCE_ABSOLUTE)
+		return
+
+	var/mob/living/carbon/human/human_holder = quirk_holder
+	human_holder.gain_trauma(new /datum/brain_trauma/mild/phobia(phobia), TRAUMA_RESILIENCE_ABSOLUTE)
 
 /datum/quirk/phobia/remove()
 	var/mob/living/carbon/human/human_holder = quirk_holder
@@ -315,7 +310,7 @@
 	/// The user's starting hairstyle
 	var/old_hair
 
-/datum/quirk/item_quirk/bald/add()
+/datum/quirk/item_quirk/bald/add(client/client_source)
 	var/mob/living/carbon/human/human_holder = quirk_holder
 	old_hair = human_holder.hairstyle
 	human_holder.hairstyle = "Bald"
@@ -323,7 +318,7 @@
 	RegisterSignal(human_holder, COMSIG_CARBON_EQUIP_HAT, PROC_REF(equip_hat))
 	RegisterSignal(human_holder, COMSIG_CARBON_UNEQUIP_HAT, PROC_REF(unequip_hat))
 
-/datum/quirk/item_quirk/bald/add_unique()
+/datum/quirk/item_quirk/bald/add_unique(client/client_source)
 	var/obj/item/clothing/head/wig/natural/baldie_wig = new(get_turf(quirk_holder))
 
 	if (old_hair == "Bald")
@@ -369,7 +364,7 @@
 	medical_record_text = "Patient mentions photography as a stress-relieving hobby."
 	mail_goodies = list(/obj/item/camera_film)
 
-/datum/quirk/item_quirk/photographer/add_unique()
+/datum/quirk/item_quirk/photographer/add_unique(client/client_source)
 	var/mob/living/carbon/human/human_holder = quirk_holder
 	var/obj/item/storage/photo_album/personal/photo_album = new(get_turf(human_holder))
 	photo_album.persistence_id = "personal_[human_holder.last_mind?.key]" // this is a persistent album, the ID is tied to the account's key to avoid tampering
@@ -396,7 +391,7 @@
 	medical_record_text = "Patient enjoys dyeing their hair with pretty colors."
 	mail_goodies = list(/obj/item/dyespray)
 
-/datum/quirk/item_quirk/colorist/add_unique()
+/datum/quirk/item_quirk/colorist/add_unique(client/client_source)
 	give_item_to_holder(/obj/item/dyespray, list(LOCATION_BACKPACK = ITEM_SLOT_BACKPACK, LOCATION_HANDS = ITEM_SLOT_HANDS))
 
 #define GAMING_WITHDRAWAL_TIME (15 MINUTES)
@@ -413,7 +408,7 @@
 	/// Timer for gaming withdrawal to kick in
 	var/gaming_withdrawal_timer = TIMER_ID_NULL
 
-/datum/quirk/gamer/add()
+/datum/quirk/gamer/add(client/client_source)
 	// Gamer diet
 	var/mob/living/carbon/human/human_holder = quirk_holder
 	var/datum/species/species = human_holder.dna.species
@@ -436,7 +431,7 @@
 	UnregisterSignal(human_holder, COMSIG_MOB_LOST_VIDEOGAME)
 	UnregisterSignal(human_holder, COMSIG_MOB_PLAYED_VIDEOGAME)
 
-/datum/quirk/gamer/add_unique()
+/datum/quirk/gamer/add_unique(client/client_source)
 	// The gamer starts off quelled
 	gaming_withdrawal_timer = addtimer(CALLBACK(src, PROC_REF(enter_withdrawal)), GAMING_WITHDRAWAL_TIME, TIMER_STOPPABLE)
 

code/__DEFINES/quirks.dm
@@ -1,0 +1,14 @@
+//Medical Categories for quirks
+#define CAT_QUIRK_ALL 0
+#define CAT_QUIRK_NOTES 1
+#define CAT_QUIRK_MINOR_DISABILITY 2
+#define CAT_QUIRK_MAJOR_DISABILITY 3
+
+/// This quirk can only be applied to humans
+#define QUIRK_HUMAN_ONLY (1<<0)
+/// This quirk processes on SSquirks (and should implement quirk process)
+#define QUIRK_PROCESSES (1<<1)
+/// This quirk is has a visual aspect in that it changes how the player looks. Used in generating dummies.
+#define QUIRK_CHANGES_APPEARANCE (1<<2)
+/// The only thing this quirk effects is mood so it should be disabled if mood is
+#define QUIRK_MOODLET_BASED (1<<3)

code/__DEFINES/traits.dm
@@ -687,12 +687,6 @@ Remember to update _globalvars/traits.dm if you're adding/removing/renaming trai
 /// Receives echolocation images.
 #define TRAIT_ECHOLOCATION_RECEIVER "echolocation_receiver"
 
-//Medical Categories for quirks
-#define CAT_QUIRK_ALL 0
-#define CAT_QUIRK_NOTES 1
-#define CAT_QUIRK_MINOR_DISABILITY 2
-#define CAT_QUIRK_MAJOR_DISABILITY 3
-
 // common trait sources
 #define TRAIT_GENERIC "generic"
 #define UNCONSCIOUS_TRAIT "unconscious"

code/controllers/subsystem/processing/quirks.dm
@@ -61,19 +61,19 @@ PROCESSING_SUBSYSTEM_DEF(quirks)
 			continue
 		hardcore_quirks[quirk_type] += hardcore_value
 
-/datum/controller/subsystem/processing/quirks/proc/AssignQuirks(mob/living/user, client/cli)
+/datum/controller/subsystem/processing/quirks/proc/AssignQuirks(mob/living/user, client/applied_client)
 	var/badquirk = FALSE
-	for(var/quirk_name in cli.prefs.all_quirks)
-		var/datum/quirk/Q = quirks[quirk_name]
-		if(Q)
-			if(user.add_quirk(Q))
+	for(var/quirk_name in applied_client.prefs.all_quirks)
+		var/datum/quirk/quirk_type = quirks[quirk_name]
+		if(ispath(quirk_type))
+			if(user.add_quirk(quirk_type, override_client = applied_client))
 				SSblackbox.record_feedback("nested tally", "quirks_taken", 1, list("[quirk_name]"))
 		else
-			stack_trace("Invalid quirk \"[quirk_name]\" in client [cli.ckey] preferences")
-			cli.prefs.all_quirks -= quirk_name
+			stack_trace("Invalid quirk \"[quirk_name]\" in client [applied_client.ckey] preferences")
+			applied_client.prefs.all_quirks -= quirk_name
 			badquirk = TRUE
 	if(badquirk)
-		cli.prefs.save_character()
+		applied_client.prefs.save_character()
 
 /*
  *Randomises the quirks for a specified mob
@@ -154,7 +154,7 @@ PROCESSING_SUBSYSTEM_DEF(quirks)
 		if (isnull(quirk))
 			continue
 
-		if (initial(quirk.mood_quirk) && CONFIG_GET(flag/disable_human_mood))
+		if ((initial(quirk.quirk_flags) & QUIRK_MOODLET_BASED) && CONFIG_GET(flag/disable_human_mood))
 			continue
 
 		var/blacklisted = FALSE

code/datums/quirks/_quirk.dm
@@ -1,20 +1,29 @@
 //every quirk in this folder should be coded around being applied on spawn
 //these are NOT "mob quirks" like GOTTAGOFAST, but exist as a medium to apply them and other different effects
 /datum/quirk
+	/// The name of the quirk
 	var/name = "Test Quirk"
+	/// The description of the quirk
 	var/desc = "This is a test quirk."
+	/// What the quirk is worth in preferences, zero = neutral / free
 	var/value = 0
-	var/human_only = TRUE
-	var/gain_text
-	var/lose_text
-	var/medical_record_text //This text will appear on medical records for the trait. Not yet implemented
-	var/mood_quirk = FALSE //if true, this quirk affects mood and is unavailable if moodlets are disabled
-	var/mob_trait //if applicable, apply and remove this mob trait
-	/// Amount of points this trait is worth towards the hardcore character mode; minus points implies a positive quirk, positive means its hard. This is used to pick the quirks assigned to a hardcore character. 0 means its not available to hardcore draws.
-	var/hardcore_value = 0
+	/// Flags related to this quirk.
+	var/quirk_flags = QUIRK_HUMAN_ONLY
+	/// Reference to the mob currently tied to this quirk datum. Quirks are not singletons.
 	var/mob/living/quirk_holder
-	/// This quirk should START_PROCESSING when added and STOP_PROCESSING when removed.
-	var/processing_quirk = FALSE
+	/// Text displayed when this quirk is assigned to a mob (and not transferred)
+	var/gain_text
+	/// Text displayed when this quirk is removed from a mob (and not transferred)
+	var/lose_text
+	///This text will appear on medical records for the trait.
+	var/medical_record_text
+	/// if applicable, apply and remove this mob trait
+	var/mob_trait
+	/// Amount of points this trait is worth towards the hardcore character mode.
+	/// Minus points implies a positive quirk, positive means its hard.
+	/// This is used to pick the quirks assigned to a hardcore character.
+	//// 0 means its not available to hardcore draws.
+	var/hardcore_value = 0
 	/// When making an abstract quirk (in OOP terms), don't forget to set this var to the type path for that abstract quirk.
 	var/abstract_parent_type = /datum/quirk
 	/// The icon to show in the preferences menu.
@@ -45,11 +54,11 @@
  * * new_holder - The mob to add this quirk to.
  * * quirk_transfer - If this is being added to the holder as part of a quirk transfer. Quirks can use this to decide not to spawn new items or apply any other one-time effects.
  */
-/datum/quirk/proc/add_to_holder(mob/living/new_holder, quirk_transfer = FALSE)
+/datum/quirk/proc/add_to_holder(mob/living/new_holder, quirk_transfer = FALSE, client/client_source)
 	if(!new_holder)
 		CRASH("Quirk attempted to be added to null mob.")
 
-	if(human_only && !ishuman(new_holder))
+	if((quirk_flags & QUIRK_HUMAN_ONLY) && !ishuman(new_holder))
 		CRASH("Human only quirk attempted to be added to non-human mob.")
 
 	if(new_holder.has_quirk(type))
@@ -60,19 +69,21 @@
 
 	quirk_holder = new_holder
 	quirk_holder.quirks += src
+	// If we weren't passed a client source try to use a present one
+	client_source ||= quirk_holder.client
 
 	if(mob_trait)
 		ADD_TRAIT(quirk_holder, mob_trait, QUIRK_TRAIT)
 
-	add()
+	add(client_source)
 
-	if(processing_quirk)
+	if(quirk_flags & QUIRK_PROCESSES)
 		START_PROCESSING(SSquirks, src)
 
 	if(!quirk_transfer)
 		if(gain_text)
 			to_chat(quirk_holder, gain_text)
-		add_unique()
+		add_unique(client_source)
 
 		if(quirk_holder.client)
 			post_add()
@@ -98,7 +109,7 @@
 	if(mob_trait)
 		REMOVE_TRAIT(quirk_holder, mob_trait, QUIRK_TRAIT)
 
-	if(processing_quirk)
+	if(quirk_flags & QUIRK_PROCESSES)
 		STOP_PROCESSING(SSquirks, src)
 
 	remove()
@@ -119,13 +130,23 @@
 		post_add()
 
 /// Any effect that should be applied every single time the quirk is added to any mob, even when transferred.
-/datum/quirk/proc/add()
-/// Any effects from the proc that should not be done multiple times if the quirk is transferred between mobs. Put stuff like spawning items in here.
-/datum/quirk/proc/add_unique()
+/datum/quirk/proc/add(client/client_source)
+	return
+
+/// Any effects from the proc that should not be done multiple times if the quirk is transferred between mobs.
+/// Put stuff like spawning items in here.
+/datum/quirk/proc/add_unique(client/client_source)
+	return
+
 /// Removal of any reversible effects added by the quirk.
 /datum/quirk/proc/remove()
-/// Any special effects or chat messages which should be applied. This proc is guaranteed to run if the mob has a client when the quirk is added. Otherwise, it runs once on the next COMSIG_MOB_LOGIN.
+	return
+
+/// Any special effects or chat messages which should be applied.
+/// This proc is guaranteed to run if the mob has a client when the quirk is added.
+/// Otherwise, it runs once on the next COMSIG_MOB_LOGIN.
 /datum/quirk/proc/post_add()
+	return
 
 /// Subtype quirk that has some bonus logic to spawn items for the player.
 /datum/quirk/item_quirk
@@ -208,12 +229,13 @@
 		return medical ? "No issues have been declared." : "None"
 	return medical ?  dat.Join("<br>") : dat.Join(", ")
 
-/mob/living/proc/cleanse_trait_datums() //removes all trait datums
-	for(var/V in quirks)
-		var/datum/quirk/T = V
-		qdel(T)
+/mob/living/proc/cleanse_quirk_datums() //removes all trait datums
+	QDEL_LIST(quirks)
 
-/mob/living/proc/transfer_trait_datums(mob/living/to_mob)
+/mob/living/proc/transfer_quirk_datums(mob/living/to_mob)
+	// We could be done before the client was moved or after the client was moved
+	var/datum/preferences/to_pass = client || to_mob.client
+
 	for(var/datum/quirk/quirk as anything in quirks)
 		quirk.remove_from_current_holder(quirk_transfer = TRUE)
-		quirk.add_to_holder(to_mob, quirk_transfer = TRUE)
+		quirk.add_to_holder(to_mob, quirk_transfer = TRUE, client_source = to_pass)

commit 07a90e578e824c203b47f9508049e341326f6960

Fixes pin quirk not working roundstart (#72255)
Broke in #72158

My fault, @iwishforducks caught it early but it just didn't register for
me

:cl:
fix: Pride pin quirk works roundstart
/:cl:

code/datums/quirks/neutral_quirks.dm
@@ -501,10 +501,10 @@
 	lose_text = "<span class='danger'>You feel only slightly less fruity than before.</span>"
 	medical_record_text = "Patient appears to be fruity."
 
-/datum/quirk/item_quirk/pride_pin/add_unique()
+/datum/quirk/item_quirk/pride_pin/add_unique(client/client_source)
 	var/obj/item/clothing/accessory/pride/pin = new(get_turf(quirk_holder))
 
-	var/pride_choice = quirk_holder.client?.prefs?.read_preference(/datum/preference/choiced/pride_pin) || assoc_to_keys(GLOB.pride_pin_reskins)[1]
+	var/pride_choice = client_source?.prefs?.read_preference(/datum/preference/choiced/pride_pin) || assoc_to_keys(GLOB.pride_pin_reskins)[1]
 	var/pride_reskin = GLOB.pride_pin_reskins[pride_choice]
 
 	pin.current_skin = pride_choice