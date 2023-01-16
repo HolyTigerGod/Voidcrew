HolyTigerGod/Voidcrew

commit ae1ab3f33f8f28aad674a1e179555aa535d4ab15

Improves Damage Detection for Maintenance adapted eyes. (#72700)
## About The Pull Request

Fixes the eyes by using the code timothymtorres suggested.

Changes an IF check to better detect light and apply damage to
maintenance adapted eyes.

fixes #59285

## Why It's Good For The Game

Fixes a buggy light sensitivity issue.

## Changelog

:cl:
fix: improved maintenance eyes light detection.
/:cl:

code/modules/surgery/organs/eyes.dm
@@ -563,9 +563,7 @@
 	ADD_TRAIT(adapted, TRAIT_UNNATURAL_RED_GLOWY_EYES, ORGAN_TRAIT)
 
 /obj/item/organ/internal/eyes/night_vision/maintenance_adapted/on_life(delta_time, times_fired)
-	var/turf/owner_turf = get_turf(owner)
-	var/lums = owner_turf.get_lumcount()
-	if(lums > 0.5) //we allow a little more than usual so we can produce light from the adapted eyes
+	if(!HAS_TRAIT(owner, TRAIT_BLIND) && isturf(owner.loc) && owner.has_light_nearby(light_amount=0.5)) //we allow a little more than usual so we can produce light from the adapted eyes
 		to_chat(owner, span_danger("Your eyes! They burn in the light!"))
 		applyOrganDamage(10) //blind quickly
 		playsound(owner, 'sound/machines/grill/grillsizzle.ogg', 50)

commit effacc97eef5dcb414d6dcac6a4ae8d39df87059

Fixing compile issue regarding Maintenance adapted eyes introduced by #72700 (#72761)
## About The Pull Request

#72700 made reference to a trait that doesn't actually exist, I've
swapped it out with the correct thingy.
## Why It's Good For The Game

To be honest its not, this fix allows the code to compile which means we
will continue playing this god forsaken game.
## Changelog
:cl:
fix: Maintenance adapted eyes no-longer prevent the game from compiling
/:cl:

code/modules/surgery/organs/eyes.dm
@@ -563,7 +563,7 @@
 	ADD_TRAIT(adapted, TRAIT_UNNATURAL_RED_GLOWY_EYES, ORGAN_TRAIT)
 
 /obj/item/organ/internal/eyes/night_vision/maintenance_adapted/on_life(delta_time, times_fired)
-	if(!HAS_TRAIT(owner, TRAIT_BLIND) && isturf(owner.loc) && owner.has_light_nearby(light_amount=0.5)) //we allow a little more than usual so we can produce light from the adapted eyes
+	if(!owner.is_blind() && isturf(owner.loc) && owner.has_light_nearby(light_amount=0.5)) //we allow a little more than usual so we can produce light from the adapted eyes
 		to_chat(owner, span_danger("Your eyes! They burn in the light!"))
 		applyOrganDamage(10) //blind quickly
 		playsound(owner, 'sound/machines/grill/grillsizzle.ogg', 50)