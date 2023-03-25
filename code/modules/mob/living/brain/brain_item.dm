HolyTigerGod/Voidcrew

commit 351713ba95f2820422e01191ca9a1b97ec4b8122

Organ Unit Tests & Bugfixes (#73026)
## About The Pull Request

This PR adds a new unit test for all organs, a new unit test for lungs,
and includes improvements for the existing breath and organ_set_bonus
tests. Using the tests, I was able to root out bugs in the organs. This
PR includes an advanced refactor of several developer-facing functions.
This PR certainly represents a "quality pass" for organs which will make
them easier to develop from now on.

### Synopsis of changes:
1. Fixed many fundamental bugs in organ code, especially in
`Insert()`/`Remove()` and their overrides.
2. Added two new procs to `/obj/item/organ` named `on_insert` and
`on_remove`, each being called after `Insert()`/`Remove()`.
3. Added `organ_effects` lazylist to `/obj/item/organ`. Converted
`organ_traits` to lazylist. 2x less empty lists per organ.
4. Adding `SHOULD_CALL_PARENT(TRUE)` to `Insert()`/`Remove()` was very
beneficial to stability and overall code health.
5. Created unit test `organ_sanity` for all usable organs in the game.
Tests insertion and removal.
6. Created unit test `lungs_sanity` for
`/obj/item/organ/internal/lungs`.
7. Improved `breath_sanity` unit tests with additional tests and
conditions.
8. Improved `organ_set_bonus_sanity` unit tests with better
documentation and maintainable code.

---

### Granular bug/fix list:

- A lot of organs are overriding `Insert()` to apply unique
side-effects, but aren't checking the return value of the parent proc
which causes the activation of side-effects even if the insertion
technically fails. I noticed the use-case of applying "unique
side-effects" is repeated across a lot of organs in the game, and by
overriding `Insert()` the potential for bugs is very high; I solved this
problem with inversion-of-control by adding two new procs to
`/obj/item/organ` named `on_insert` and `on_remove`, each being called
after `Insert()` and `Remove()` succeed.

code/modules/mob/living/brain/brain_item.dm
@@ -39,57 +39,59 @@
 	/// Maximum skillchip slots available. Do not reference this var directly and instead call get_max_skillchip_slots()
 	var/max_skillchip_slots = 5
 
-/obj/item/organ/internal/brain/Insert(mob/living/carbon/C, special = FALSE, drop_if_replaced = TRUE, no_id_transfer = FALSE)
+/obj/item/organ/internal/brain/Insert(mob/living/carbon/brain_owner, special = FALSE, drop_if_replaced = TRUE, no_id_transfer = FALSE)
 	. = ..()
+	if(!.)
+		return
 
 	name = initial(name)
 
-	if(C.mind && C.mind.has_antag_datum(/datum/antagonist/changeling) && !no_id_transfer) //congrats, you're trapped in a body you don't control
-		if(brainmob && !(C.stat == DEAD || (HAS_TRAIT(C, TRAIT_DEATHCOMA))))
+	if(brain_owner.mind && brain_owner.mind.has_antag_datum(/datum/antagonist/changeling) && !no_id_transfer) //congrats, you're trapped in a body you don't control
+		if(brainmob && !(brain_owner.stat == DEAD || (HAS_TRAIT(brain_owner, TRAIT_DEATHCOMA))))
 			to_chat(brainmob, span_danger("You can't feel your body! You're still just a brain!"))
-		forceMove(C)
-		C.update_body_parts()
+		forceMove(brain_owner)
+		brain_owner.update_body_parts()
 		return
 
 	if(brainmob)
-		if(C.key)
-			C.ghostize()
+		if(brain_owner.key)
+			brain_owner.ghostize()
 
 		if(brainmob.mind)
-			brainmob.mind.transfer_to(C)
+			brainmob.mind.transfer_to(brain_owner)
 		else
-			C.key = brainmob.key
+			brain_owner.key = brainmob.key
 
-		C.set_suicide(brainmob.suiciding)
+		brain_owner.set_suicide(brainmob.suiciding)
 
 		QDEL_NULL(brainmob)
 
 	else
-		C.set_suicide(suicided)
+		brain_owner.set_suicide(suicided)
 
 	for(var/datum/brain_trauma/trauma as anything in traumas)
 		if(trauma.owner)
-			if(trauma.owner == owner)
+			if(trauma.owner == brain_owner)
 				// if we're being special replaced, the trauma is already applied, so this is expected
 				// but if we're not... this is likely a bug, and should be reported
 				if(!special)
-					stack_trace("A brain trauma ([trauma]) is being re-applied to its owning mob ([owner])!")
+					stack_trace("A brain trauma ([trauma]) is being re-applied to its owning mob ([brain_owner])!")
 				continue
 
-			stack_trace("A brain trauma ([trauma]) is being applied to a new mob ([owner]) when it's owned by someone else ([trauma.owner])!")
+			stack_trace("A brain trauma ([trauma]) is being applied to a new mob ([brain_owner]) when it's owned by someone else ([trauma.owner])!")
 			continue
 
-		trauma.owner = owner
+		trauma.owner = brain_owner
 		trauma.on_gain()
 
 	//Update the body's icon so it doesnt appear debrained anymore
-	C.update_body_parts()
+	brain_owner.update_body_parts()
 
-/obj/item/organ/internal/brain/Remove(mob/living/carbon/C, special = 0, no_id_transfer = FALSE)
+/obj/item/organ/internal/brain/Remove(mob/living/carbon/brain_owner, special = 0, no_id_transfer = FALSE)
 	// Delete skillchips first as parent proc sets owner to null, and skillchips need to know the brain's owner.
-	if(!QDELETED(C) && length(skillchips))
+	if(!QDELETED(brain_owner) && length(skillchips))
 		if(!special)
-			to_chat(C, span_notice("You feel your skillchips enable emergency power saving mode, deactivating as your brain leaves your body..."))
+			to_chat(brain_owner, span_notice("You feel your skillchips enable emergency power saving mode, deactivating as your brain leaves your body..."))
 		for(var/chip in skillchips)
 			var/obj/item/skillchip/skillchip = chip
 			// Run the try_ proc with force = TRUE.
@@ -103,9 +105,9 @@
 		BT.owner = null
 
 	if((!gc_destroyed || (owner && !owner.gc_destroyed)) && !no_id_transfer)
-		transfer_identity(C)
-	C.update_body_parts()
-	C.clear_mood_event("brain_damage")
+		transfer_identity(brain_owner)
+	brain_owner.update_body_parts()
+	brain_owner.clear_mood_event("brain_damage")
 
 /obj/item/organ/internal/brain/proc/transfer_identity(mob/living/L)
 	name = "[L.name]'s [initial(name)]"

code/__DEFINES/traits.dm
@@ -736,6 +736,8 @@ Remember to update _globalvars/traits.dm if you're adding/removing/renaming trai
 #define DISEASE_TRAIT "disease"
 #define SPECIES_TRAIT "species"
 #define ORGAN_TRAIT "organ"
+/// Trait given by organ gained via abductor surgery
+#define ABDUCTOR_GLAND_TRAIT "abductor_gland"
 /// cannot be removed without admin intervention
 #define ROUNDSTART_TRAIT "roundstart"
 #define JOB_TRAIT "job"

code/game/machinery/dna_infuser/organ_sets/carp_organs.dm
@@ -48,7 +48,7 @@
 	AddElement(/datum/element/noticable_organ, "has big sharp teeth.", BODY_ZONE_PRECISE_MOUTH)
 	AddElement(/datum/element/organ_set_bonus, /datum/status_effect/organ_set_bonus/carp)
 
-/obj/item/organ/internal/tongue/carp/Insert(mob/living/carbon/tongue_owner, special, drop_if_replaced)
+/obj/item/organ/internal/tongue/carp/on_insert(mob/living/carbon/tongue_owner)
 	. = ..()
 	if(!ishuman(tongue_owner))
 		return
@@ -62,7 +62,7 @@
 	head.unarmed_damage_high = 15
 	head.unarmed_stun_threshold = 15
 
-/obj/item/organ/internal/tongue/carp/Remove(mob/living/carbon/tongue_owner, special)
+/obj/item/organ/internal/tongue/carp/on_remove(mob/living/carbon/tongue_owner)
 	. = ..()
 	if(!ishuman(tongue_owner))
 		return
@@ -110,13 +110,13 @@
 	AddElement(/datum/element/organ_set_bonus, /datum/status_effect/organ_set_bonus/carp)
 	AddElement(/datum/element/noticable_organ, "seem%PRONOUN_S unable to stay still.")
 
-/obj/item/organ/internal/brain/carp/Insert(mob/living/carbon/brain_owner, special, drop_if_replaced, no_id_transfer)
+/obj/item/organ/internal/brain/carp/on_insert(mob/living/carbon/brain_owner)
 	. = ..()
 	cooldown_timer = addtimer(CALLBACK(src, PROC_REF(unsatisfied_nomad)), cooldown_time, TIMER_STOPPABLE|TIMER_OVERRIDE|TIMER_UNIQUE)
 	RegisterSignal(brain_owner, COMSIG_MOVABLE_Z_CHANGED, PROC_REF(satisfied_nomad))
 
 //technically you could get around the mood issue by extracting and reimplanting the brain but it will be far easier to just go one z there and back
-/obj/item/organ/internal/brain/carp/Remove(mob/living/carbon/brain_owner, special, no_id_transfer)
+/obj/item/organ/internal/brain/carp/on_remove(mob/living/carbon/brain_owner)
 	. = ..()
 	UnregisterSignal(brain_owner, COMSIG_MOVABLE_Z_CHANGED)
 	deltimer(cooldown_timer)

code/game/machinery/dna_infuser/organ_sets/fly_organs.dm
@@ -10,7 +10,7 @@
 
 /datum/status_effect/organ_set_bonus/fly/enable_bonus()
 	. = ..()
-	if(!ishuman(owner))
+	if(!. || !ishuman(owner))
 		return
 	var/mob/living/carbon/human/new_fly = owner
 	if(isflyperson(new_fly))

code/game/machinery/dna_infuser/organ_sets/fox_organs.dm
@@ -6,7 +6,7 @@
 	visual = TRUE
 	damage_multiplier = 2
 
-/obj/item/organ/internal/ears/fox/Insert(mob/living/carbon/human/ear_owner, special = 0, drop_if_replaced = TRUE)
+/obj/item/organ/internal/ears/fox/on_insert(mob/living/carbon/human/ear_owner)
 	. = ..()
 	if(istype(ear_owner) && ear_owner.dna)
 		color = ear_owner.hair_color
@@ -14,7 +14,7 @@
 		ear_owner.dna.update_uf_block(DNA_EARS_BLOCK)
 		ear_owner.update_body()
 
-/obj/item/organ/internal/ears/fox/Remove(mob/living/carbon/human/ear_owner,  special = 0)
+/obj/item/organ/internal/ears/fox/on_remove(mob/living/carbon/human/ear_owner)
 	. = ..()
 	if(istype(ear_owner) && ear_owner.dna)
 		color = ear_owner.hair_color

code/game/machinery/dna_infuser/organ_sets/goliath_organs.dm
@@ -66,7 +66,7 @@
 	AddElement(/datum/element/noticable_organ, "arm is just a mass of plate and tendrils.", BODY_ZONE_CHEST)
 	AddElement(/datum/element/organ_set_bonus, /datum/status_effect/organ_set_bonus/goliath)
 
-/obj/item/organ/internal/brain/goliath/Insert(mob/living/carbon/brain_owner, special, drop_if_replaced, no_id_transfer)
+/obj/item/organ/internal/brain/goliath/on_insert(mob/living/carbon/brain_owner)
 	. = ..()
 	if(!ishuman(brain_owner))
 		return
@@ -78,7 +78,7 @@
 	hammer = new/obj/item/goliath_infuser_hammer
 	brain_owner.put_in_hands(hammer)
 
-/obj/item/organ/internal/brain/goliath/Remove(mob/living/carbon/brain_owner, special, no_id_transfer)
+/obj/item/organ/internal/brain/goliath/on_remove(mob/living/carbon/brain_owner)
 	. = ..()
 	UnregisterSignal(brain_owner)
 	if(!ishuman(brain_owner))

code/game/machinery/dna_infuser/organ_sets/rat_organs.dm
@@ -50,7 +50,7 @@
 	AddElement(/datum/element/organ_set_bonus, /datum/status_effect/organ_set_bonus/rat)
 	AddElement(/datum/element/noticable_organ, "mouth is drooling excessively.", BODY_ZONE_PRECISE_MOUTH)
 
-/obj/item/organ/internal/stomach/rat/Insert(mob/living/carbon/receiver, special, drop_if_replaced)
+/obj/item/organ/internal/stomach/rat/on_insert(mob/living/carbon/receiver)
 	. = ..()
 	if(!ishuman(receiver))
 		return
@@ -73,7 +73,7 @@
 	new_species.disliked_food = NONE
 	new_species.toxic_food = NONE
 
-/obj/item/organ/internal/stomach/rat/Remove(mob/living/carbon/stomach_owner, special)
+/obj/item/organ/internal/stomach/rat/on_remove(mob/living/carbon/stomach_owner)
 	. = ..()
 	if(!ishuman(stomach_owner))
 		return
@@ -103,9 +103,9 @@
 	AddElement(/datum/element/organ_set_bonus, /datum/status_effect/organ_set_bonus/rat)
 	AddElement(/datum/element/noticable_organ, "hunch%PRONOUN_ES over unnaturally!")
 
-/obj/item/organ/internal/heart/rat/Insert(mob/living/carbon/receiver, special, drop_if_replaced)
+/obj/item/organ/internal/heart/rat/on_insert(mob/living/carbon/receiver)
 	. = ..()
-	if(!ishuman(receiver))
+	if(!. || !ishuman(receiver))
 		return
 	var/mob/living/carbon/human/human_receiver = receiver
 	if(!human_receiver.can_mutate())
@@ -115,7 +115,7 @@
 	if(human_receiver.physiology)
 		human_receiver.physiology.damage_resistance -= 50
 
-/obj/item/organ/internal/heart/rat/Remove(mob/living/carbon/heartless, special)
+/obj/item/organ/internal/heart/rat/on_remove(mob/living/carbon/heartless, special)
 	. = ..()
 	if(!ishuman(heartless))
 		return
@@ -151,11 +151,11 @@
 	if(message == "hi?")
 		speech_args[SPEECH_MESSAGE] = "Um... cheesed to meet you?"
 
-/obj/item/organ/internal/tongue/rat/Insert(mob/living/carbon/tongue_owner, special, drop_if_replaced)
+/obj/item/organ/internal/tongue/rat/on_insert(mob/living/carbon/tongue_owner, special, drop_if_replaced)
 	. = ..()
 	RegisterSignal(tongue_owner, COMSIG_CARBON_ITEM_GIVEN, PROC_REF(its_on_the_mouse))
 
-/obj/item/organ/internal/tongue/rat/Remove(mob/living/carbon/tongue_owner, special)
+/obj/item/organ/internal/tongue/rat/on_remove(mob/living/carbon/tongue_owner)
 	. = ..()
 	UnregisterSignal(tongue_owner, COMSIG_CARBON_ITEM_GIVEN)
 

code/game/objects/items/body_egg.dm
@@ -15,18 +15,19 @@
 	if(iscarbon(loc))
 		Insert(loc)
 
-/obj/item/organ/internal/body_egg/Insert(mob/living/carbon/M, special = FALSE, drop_if_replaced = TRUE)
+/obj/item/organ/internal/body_egg/Insert(mob/living/carbon/egg_owner, special = FALSE, drop_if_replaced = TRUE)
 	. = ..()
-	owner.add_traits(list(TRAIT_XENO_HOST, TRAIT_XENO_IMMUNE), ORGAN_TRAIT)
-	owner.med_hud_set_status()
-	INVOKE_ASYNC(src, PROC_REF(AddInfectionImages), owner)
+	if(!.)
+		return
+	egg_owner.add_traits(list(TRAIT_XENO_HOST, TRAIT_XENO_IMMUNE), ORGAN_TRAIT)
+	egg_owner.med_hud_set_status()
+	INVOKE_ASYNC(src, PROC_REF(AddInfectionImages), egg_owner)
 
-/obj/item/organ/internal/body_egg/Remove(mob/living/carbon/M, special = FALSE)
-	if(owner)
-		owner.remove_traits(list(TRAIT_XENO_HOST, TRAIT_XENO_IMMUNE), ORGAN_TRAIT)
-		owner.med_hud_set_status()
-		INVOKE_ASYNC(src, PROC_REF(RemoveInfectionImages), owner)
-	return ..()
+/obj/item/organ/internal/body_egg/Remove(mob/living/carbon/egg_owner, special = FALSE)
+	. = ..()
+	egg_owner.remove_traits(list(TRAIT_XENO_HOST, TRAIT_XENO_IMMUNE), ORGAN_TRAIT)
+	egg_owner.med_hud_set_status()
+	INVOKE_ASYNC(src, PROC_REF(RemoveInfectionImages), egg_owner)
 
 /obj/item/organ/internal/body_egg/on_death(delta_time, times_fired)
 	. = ..()

commit 94852a27aa14df38c9f8beb2b9b3df4a941d9002

Fixes cliented brainmobs being sent to the gem room when their amputated head is reattached to a body. (#74227)
## About The Pull Request

Fixes #74126

Hi vsauce, Timberpoes here!

Brains are being sent the gem room. But why???? *musical sting*

Issue was caused by #73026

![image](https://user-images.githubusercontent.com/24975989/227698989-76e96198-b000-44d5-8c71-65721f25f9df.png)

This early return added to brain Insert is the root cause.

Let's have a butcher's at some code, eh?


![image](https://user-images.githubusercontent.com/24975989/227699118-217bc7a2-160c-41a0-b75e-ef389c43f538.png)

Here we have a parent call in `. = ..()` and then a check for brains and
brainmobs to do some special snowflake stuff before
`brain.Insert(new_head_owner)`.

This causes a problem. But WHY???????

Well... The parent call already inserts the brain, but without any
snowflake stuff done.


![image](https://user-images.githubusercontent.com/24975989/227699193-2bb29dce-c92d-4253-aa78-4f771d765bf2.png)

The parent call returns and the child proc
`/obj/item/bodypart/head/try_attach_limb()` which mashes the brainmob
into the brain and slams the `brain.Insert()` proc a **second** time.

This is a bug. But it worked despite being a bug, because the brain's
Insert proc would still do logic even if the Insert was technically not
successful.

But... We come back to this again:

![image](https://user-images.githubusercontent.com/24975989/227701040-a98b0ae7-1221-431a-8cc5-d47061372f7a.png)

If the new early return for the parent call is FALSEy, it stops all that
wonderful brainmob code from being executed.


![image](https://user-images.githubusercontent.com/24975989/227699367-c6aafcf1-8c50-41fe-b90d-820cd8cd1618.png)

... And ITS parent call is FALSE because `owner == receiver` is now
TRUE, as it was set the first time `Insert()` was called all the way
back in `/obj/item/bodypart/proc/try_attach_limb()`.

code/modules/mob/living/brain/brain_item.dm
@@ -44,6 +44,16 @@
 	if(!.)
 		return
 
+	// Transfer brainmob from the head if we're being transferred from a head to a new body.
+	// And example of this ocurring is reattaching an amputated/severed head via surgery.
+	if(istype(loc, /obj/item/bodypart/head))
+		var/obj/item/bodypart/head/brain_holder = loc
+		if(brain_holder.brainmob)
+			brainmob = brain_holder.brainmob
+			brain_holder.brainmob = null
+			brainmob.container = null
+			brainmob.forceMove(src)
+
 	name = initial(name)
 
 	if(brain_owner.mind && brain_owner.mind.has_antag_datum(/datum/antagonist/changeling) && !no_id_transfer) //congrats, you're trapped in a body you don't control

code/modules/surgery/bodyparts/dismemberment.dm
@@ -373,18 +373,12 @@
 	var/real_name = src.real_name
 
 	. = ..()
-	if(!.)
-		return .
-	//Transfer some head appearance vars over
-	if(brain)
-		if(brainmob)
-			brainmob.container = null //Reset brainmob head var.
-			brainmob.forceMove(brain) //Throw mob into brain.
-			brain.brainmob = brainmob //Set the brain to use the brainmob
-			brainmob = null //Set head brainmob var to null
-		brain.Insert(new_head_owner) //Now insert the brain proper
-		brain = null //No more brain in the head
 
+	if(!.)
+		return
+
+	if(brain)
+		brain = null
 	if(tongue)
 		tongue = null
 	if(ears)