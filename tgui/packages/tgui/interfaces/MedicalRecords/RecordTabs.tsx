HolyTigerGod/Voidcrew

commit f9caabbab86de174f7ee270d936e26ef5ac73fea

Replaces most useLocalState with useState (#80784)
## About The Pull Request
#80459 was closed unfortunately, which had the changes I was looking
forward to. This is a continuation of that project which aims to safely
replace the simple uses of useLocalState since we have the real hook
now.
## Why It's Good For The Game
Code improvement
## Changelog
N/A nothing player facing

tgui/packages/tgui/interfaces/MedicalRecords/RecordTabs.tsx
@@ -1,6 +1,7 @@
 import { filter, sortBy } from 'common/collections';
 import { flow } from 'common/fp';
-import { useBackend, useLocalState } from 'tgui/backend';
+import { useState } from 'react';
+import { useBackend } from 'tgui/backend';
 import {
   Box,
   Button,
@@ -25,7 +26,7 @@ export const MedicalRecordTabs = (props) => {
     ? 'No records found.'
     : 'No match. Refine your search.';
 
-  const [search, setSearch] = useLocalState('search', '');
+  const [search, setSearch] = useState('');
 
   const sorted: MedicalRecord[] = flow([
     filter((record: MedicalRecord) => isRecordMatch(record, search)),
@@ -82,9 +83,9 @@ export const MedicalRecordTabs = (props) => {
 
 /** Individual crew tab */
 const CrewTab = (props: { record: MedicalRecord }) => {
-  const [selectedRecord, setSelectedRecord] = useLocalState<
+  const [selectedRecord, setSelectedRecord] = useState<
     MedicalRecord | undefined
-  >('medicalRecord', undefined);
+  >();
 
   const { act, data } = useBackend<MedicalRecordData>();
   const { assigned_view } = data;

tgui/packages/tgui/interfaces/Achievements.jsx
@@ -1,16 +1,13 @@
 import { useState } from 'react';
 
-import { useBackend, useLocalState } from '../backend';
+import { useBackend } from '../backend';
 import { Box, Flex, Icon, Table, Tabs, Tooltip } from '../components';
 import { Window } from '../layouts';
 
 export const Achievements = (props) => {
   const { data } = useBackend();
   const { categories } = data;
-  const [selectedCategory, setSelectedCategory] = useLocalState(
-    'category',
-    categories[0],
-  );
+  const [selectedCategory, setSelectedCategory] = useState(categories[0]);
   const achievements = data.achievements.filter(
     (x) => x.category === selectedCategory,
   );

tgui/packages/tgui/interfaces/AdminFax.jsx
@@ -1,4 +1,6 @@
-import { useBackend, useLocalState } from '../backend';
+import { useState } from 'react';
+
+import { useBackend } from '../backend';
 import {
   Box,
   Button,
@@ -26,15 +28,15 @@ export const AdminFax = (props) => {
 export const FaxMainPanel = (props) => {
   const { act, data } = useBackend();
 
-  const [fax, setFax] = useLocalState('fax', '');
-  const [saved, setSaved] = useLocalState('saved', false);
-  const [paperName, setPaperName] = useLocalState('paperName', '');
-  const [fromWho, setFromWho] = useLocalState('fromWho', '');
-  const [rawText, setRawText] = useLocalState('rawText', '');
-  const [stamp, setStamp] = useLocalState('stampType', '');
-  const [stampCoordX, setStampCoordX] = useLocalState('stampCoordX', 0);
-  const [stampCoordY, setStampCoordY] = useLocalState('stampCoordY', 0);
-  const [stampAngle, setStampAngle] = useLocalState('stampAngle', 0);
+  const [fax, setFax] = useState('');
+  const [saved, setSaved] = useState(false);
+  const [paperName, setPaperName] = useState('');
+  const [fromWho, setFromWho] = useState('');
+  const [rawText, setRawText] = useState('');
+  const [stamp, setStamp] = useState('');
+  const [stampCoordX, setStampCoordX] = useState(0);
+  const [stampCoordY, setStampCoordY] = useState(0);
+  const [stampAngle, setStampAngle] = useState(0);
   if (stamp && data.stamps[0] !== 'None') {
     data.stamps.unshift('None');
   }

tgui/packages/tgui/interfaces/AlertModal.tsx
@@ -1,3 +1,5 @@
+import { useState } from 'react';
+
 import {
   KEY_ENTER,
   KEY_ESCAPE,
@@ -6,7 +8,7 @@ import {
   KEY_SPACE,
   KEY_TAB,
 } from '../../common/keycodes';
-import { useBackend, useLocalState } from '../backend';
+import { useBackend } from '../backend';
 import { Autofocus, Box, Button, Flex, Section, Stack } from '../components';
 import { Window } from '../layouts';
 import { Loader } from './common/Loader';
@@ -34,7 +36,7 @@ export const AlertModal = (props) => {
     timeout,
     title,
   } = data;
-  const [selected, setSelected] = useLocalState<number>('selected', 0);
+  const [selected, setSelected] = useState(0);
   // Dynamically sets window dimensions
   const windowHeight =
     115 +

tgui/packages/tgui/interfaces/ApcControl.jsx
@@ -1,5 +1,6 @@
 import { map, sortBy } from 'common/collections';
 import { flow } from 'common/fp';
+import { useState } from 'react';
 
 import { useBackend, useLocalState } from '../backend';
 import {
@@ -47,7 +48,7 @@ const ApcLoggedOut = (props) => {
 const ApcLoggedIn = (props) => {
   const { act, data } = useBackend();
   const { restoring } = data;
-  const [tabIndex, setTabIndex] = useLocalState('tab-index', 1);
+  const [tabIndex, setTabIndex] = useState(1);
   return (
     <Box>
       <Tabs>

tgui/packages/tgui/interfaces/Biogenerator.tsx
@@ -1,5 +1,6 @@
 import { BooleanLike } from 'common/react';
 import { classes } from 'common/react';
+import { useState } from 'react';
 
 import { useBackend, useLocalState } from '../backend';
 import {
@@ -184,8 +185,7 @@ export const Biogenerator = (props) => {
 const ItemList = (props) => {
   const { act } = useBackend();
   const items = props.items.map((item) => {
-    const [amount, setAmount] = useLocalState(
-      'amount' + item.name,
+    const [amount, setAmount] = useState(
       item.is_reagent ? Math.min(Math.max(props.space, 1), 10) : 1,
     );
     const disabled =

tgui/packages/tgui/interfaces/CentcomPodLauncher.jsx
@@ -3,7 +3,7 @@ import { classes } from 'common/react';
 import { storage } from 'common/storage';
 import { multiline } from 'common/string';
 import { createUuid } from 'common/uuid';
-import { Component, Fragment } from 'react';
+import { Component, Fragment, useState } from 'react';
 
 import { useBackend, useLocalState } from '../backend';
 import {
@@ -25,7 +25,7 @@ const pod_grey = {
 };
 
 const useCompact = () => {
-  const [compact, setCompact] = useLocalState('compact', false);
+  const [compact, setCompact] = useState(false);
   const toggleCompact = () => setCompact(!compact);
   return [compact, toggleCompact];
 };
@@ -830,10 +830,10 @@ class PresetsPage extends Component {
   render() {
     const { presets } = this.state;
     const { act, data } = useBackend();
-    const [presetIndex, setSelectedPreset] = useLocalState('presetIndex', 0);
-    const [settingName, setEditingNameStatus] = useLocalState('settingName', 0);
-    const [newNameText, setText] = useLocalState('newNameText', '');
-    const [hue, setHue] = useLocalState('hue', 0);
+    const [presetIndex, setSelectedPreset] = useState(0);
+    const [settingName, setEditingNameStatus] = useState(0);
+    const [newNameText, setText] = useState('');
+    const [hue, setHue] = useState(0);
     return (
       <Section
         scrollable

tgui/packages/tgui/interfaces/CheckboxInput.tsx
@@ -1,6 +1,7 @@
 import { createSearch, decodeHtmlEntities } from 'common/string';
+import { useState } from 'react';
 
-import { useBackend, useLocalState } from '../backend';
+import { useBackend } from '../backend';
 import {
   Button,
   Icon,
@@ -37,12 +38,9 @@ export const CheckboxInput = (props) => {
     title,
   } = data;
 
-  const [selections, setSelections] = useLocalState<string[]>('selections', []);
+  const [selections, setSelections] = useState<string[]>([]);
 
-  const [searchQuery, setSearchQuery] = useLocalState<string>(
-    'searchQuery',
-    '',
-  );
+  const [searchQuery, setSearchQuery] = useState('');
   const search = createSearch(searchQuery, (item: string) => item);
   const toDisplay = items.filter(search);
 

commit 7f33d551057d8b06fbab106a9e529ece722f768f

Fixes medical records state [no gbp] (#80858)
## About The Pull Request
Caused by #80784. Component is using localstate as a global. This should
eventually be refactored
## Why It's Good For The Game
Fixes #80854 
## Changelog
:cl:
fix: Medical records console should be useable again
/:cl:

tgui/packages/tgui/interfaces/MedicalRecords/RecordTabs.tsx
@@ -1,7 +1,7 @@
 import { filter, sortBy } from 'common/collections';
 import { flow } from 'common/fp';
 import { useState } from 'react';
-import { useBackend } from 'tgui/backend';
+import { useBackend, useLocalState } from 'tgui/backend';
 import {
   Box,
   Button,
@@ -83,9 +83,9 @@ export const MedicalRecordTabs = (props) => {
 
 /** Individual crew tab */
 const CrewTab = (props: { record: MedicalRecord }) => {
-  const [selectedRecord, setSelectedRecord] = useState<
+  const [selectedRecord, setSelectedRecord] = useLocalState<
     MedicalRecord | undefined
-  >();
+  >('medicalRecord', undefined);
 
   const { act, data } = useBackend<MedicalRecordData>();
   const { assigned_view } = data;

tgui/packages/tgui/interfaces/SecurityRecords/RecordTabs.tsx
@@ -1,5 +1,6 @@
 import { filter, sortBy } from 'common/collections';
 import { flow } from 'common/fp';
+import { useState } from 'react';
 import { useBackend, useLocalState } from 'tgui/backend';
 import {
   Box,
@@ -26,7 +27,7 @@ export const SecurityRecordTabs = (props) => {
     ? 'No records found.'
     : 'No match. Refine your search.';
 
-  const [search, setSearch] = useLocalState('search', '');
+  const [search, setSearch] = useState('');
 
   const sorted: SecurityRecord[] = flow([
     filter((record: SecurityRecord) => isRecordMatch(record, search)),