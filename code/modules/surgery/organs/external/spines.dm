HolyTigerGod/Voidcrew

commit 183ed8d8a41e7b1f3e3bc09f3fbe98c7a6406f32

Turns mush cap into an extorgan (#80952)
Pretty self explanatory. I suppose this isn't a 100% fix or refactor,
since it adds a UI for their features and makes it removable, but also
these never happen

:cl:
code: The mushroom cap is now an external organ (jungle station will
never happen)
fix: Mushpeople caps are no longer solid black
/:cl:

code/modules/surgery/organs/external/spines.dm
@@ -7,8 +7,6 @@
 	zone = BODY_ZONE_CHEST
 	slot = ORGAN_SLOT_EXTERNAL_SPINES
 
-	preference = "feature_lizard_spines"
-
 	dna_block = DNA_SPINES_BLOCK
 	restyle_flags = EXTERNAL_RESTYLE_FLESH
 

code/modules/client/preferences/species_features/mushperson.dm
@@ -1,0 +1,11 @@
+/datum/preference/choiced/mushroom_cap
+	savefile_key = "feature_mushperson_cap"
+	savefile_identifier = PREFERENCE_CHARACTER
+	category = PREFERENCE_CATEGORY_SECONDARY_FEATURES
+	relevant_mutant_bodypart = "cap"
+
+/datum/preference/choiced/mushroom_cap/init_possible_values()
+	return assoc_to_keys_features(GLOB.caps_list)
+
+/datum/preference/choiced/mushroom_cap/apply_to_human(mob/living/carbon/human/target, value)
+	target.dna.features["caps"] = value

code/modules/mob/living/carbon/human/_species.dm
@@ -746,8 +746,6 @@ GLOBAL_LIST_EMPTY(features_by_species)
 					accessory = GLOB.body_markings_list[source.dna.features["body_markings"]]
 				if("legs")
 					accessory = GLOB.legs_list[source.dna.features["legs"]]
-				if("caps")
-					accessory = GLOB.caps_list[source.dna.features["caps"]]
 
 			if(!accessory || accessory.icon_state == "none")
 				continue

code/modules/mob/living/carbon/human/species_types/mushpeople.dm
@@ -2,11 +2,12 @@
 	name = "Mushroomperson"
 	plural_form = "Mushroompeople"
 	id = SPECIES_MUSHROOM
-	mutant_bodyparts = list("caps" = "Round")
 	changesource_flags = MIRROR_BADMIN | WABBAJACK | ERT_SPAWN
 
 	fixed_mut_color = "#DBBF92"
 	hair_color = "#FF4B19" //cap color, spot color uses eye color
+
+	external_organs = list(/obj/item/organ/external/mushroom_cap = "Round")
 
 	inherent_traits = list(
 		TRAIT_MUTANT_COLORS,
@@ -41,12 +42,8 @@
 /datum/species/mush/on_species_gain(mob/living/carbon/C, datum/species/old_species)
 	. = ..()
 	if(ishuman(C))
-		var/mob/living/carbon/human/H = C
-		if(!H.dna.features["caps"])
-			H.dna.features["caps"] = "Round"
-			handle_mutant_bodyparts(H)
 		mush = new(null)
-		mush.teach(H)
+		mush.teach(C)
 
 /datum/species/mush/on_species_loss(mob/living/carbon/C)
 	. = ..()
@@ -60,6 +57,34 @@
 	if(chem.type == /datum/reagent/toxin/plantbgone/weedkiller)
 		affected.adjustToxLoss(3 * REM * seconds_per_tick)
 
-/datum/species/mush/handle_mutant_bodyparts(mob/living/carbon/human/H, forced_colour)
-	forced_colour = FALSE
-	return ..()
+/// A mushpersons mushroom cap organ
+/obj/item/organ/external/mushroom_cap
+	name = "mushroom cap"
+	desc = "These are yummie, no cap."
+
+	use_mob_sprite_as_obj_sprite = TRUE
+
+	zone = BODY_ZONE_HEAD
+	slot = ORGAN_SLOT_EXTERNAL_POD_HAIR
+
+	preference = "feature_mushperson_cap"
+
+	dna_block = DNA_MUSHROOM_CAPS_BLOCK
+	restyle_flags = EXTERNAL_RESTYLE_PLANT
+
+	bodypart_overlay = /datum/bodypart_overlay/mutant/mushroom_cap
+
+/// Bodypart overlay for the mushroom cap organ
+/datum/bodypart_overlay/mutant/mushroom_cap
+	layers = EXTERNAL_ADJACENT
+	feature_key = "caps"
+
+/datum/bodypart_overlay/mutant/mushroom_cap/get_global_feature_list()
+	return GLOB.caps_list
+
+/datum/bodypart_overlay/mutant/mushroom_cap/can_draw_on_bodypart(mob/living/carbon/human/human)
+	if((human.head?.flags_inv & HIDEHAIR) || (human.wear_mask?.flags_inv & HIDEHAIR))
+		return FALSE
+
+	return TRUE
+

tgstation.dme
@@ -3629,6 +3629,7 @@
 #include "code\modules\client\preferences\species_features\felinid.dm"
 #include "code\modules\client\preferences\species_features\lizard.dm"
 #include "code\modules\client\preferences\species_features\moth.dm"
+#include "code\modules\client\preferences\species_features\mushperson.dm"
 #include "code\modules\client\preferences\species_features\mutants.dm"
 #include "code\modules\client\preferences\species_features\pod.dm"
 #include "code\modules\client\preferences\species_features\vampire.dm"

commit 717d3e7127f8eae434e16362d624d94ce3857519

Fix lizardperson spine preference dropdown not showing up (#81475)
## About The Pull Request

I was recently told about the fact that apparently spines are missing
from lizardperson customization.
Looking into it, it seems like this was caused by #80952 performing an
unrelated line removal, presumably by accident while copying something
over.

![image](https://github.com/tgstation/tgstation/assets/42909981/b7b306ed-16b9-4d27-b0db-55d3590b1003)
Re-adding this line seems to make it work fine.
## Why It's Good For The Game

Fixes the lizardperson spine preference dropdown not showing up in the
character menu.
## Changelog
:cl:
fix: Fixed the lizardperson spine preference dropdown not showing up in
the character menu.
/:cl:

code/modules/surgery/organs/external/spines.dm
@@ -7,6 +7,8 @@
 	zone = BODY_ZONE_CHEST
 	slot = ORGAN_SLOT_EXTERNAL_SPINES
 
+	preference = "feature_lizard_spines"
+
 	dna_block = DNA_SPINES_BLOCK
 	restyle_flags = EXTERNAL_RESTYLE_FLESH
 