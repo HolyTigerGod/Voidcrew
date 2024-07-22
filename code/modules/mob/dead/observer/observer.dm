tgstation/tgstation

commit dfb12f91d67b767bb8563b40f2b69985d45373e8

HUD traits now apply their corresponding hud automatically. Most clothing/item/etc sources of HUDs now only use traits (#84984)
## About The Pull Request

Currently if you want to apply a HUD you usually add both its trait and
the HUD itself. Only exceptions are things like simplemobs where you
should avoid adding the hud trait since it adds security/med DB access
and such, but there is no cases where you'd want to apply the trait and
not apply the hud.

Requested by Melbert about a week ago.

![image](https://github.com/user-attachments/assets/8af3e9cc-ea22-4cee-86ec-54c397291727)

## Why It's Good For The Game

This makes working with HUDs significantly easier, as you no longer have
to bother with manually adding/removing them. Also potentially removes
an edge case where if your hud could get removed while keeping the
trait.

## Changelog
:cl:
refactor: HUD traits now apply their corresponding hud automatically
/:cl:

code/modules/mob/dead/observer/observer.dm
@@ -36,7 +36,6 @@ GLOBAL_VAR_INIT(observer_default_invisibility, INVISIBILITY_OBSERVER)
 	var/health_scan = FALSE //Are health scans currently enabled?
 	var/chem_scan = FALSE //Are chem scans currently enabled?
 	var/gas_scan = FALSE //Are gas scans currently enabled?
-	var/list/datahuds = list(DATA_HUD_SECURITY_ADVANCED, DATA_HUD_MEDICAL_ADVANCED, DATA_HUD_DIAGNOSTIC_ADVANCED) //list of data HUDs shown to ghosts.
 	var/ghost_orbit = GHOST_ORBIT_CIRCLE
 
 	//These variables store hair data if the ghost originates from a species with head and/or facial hair.
@@ -62,6 +61,13 @@ GLOBAL_VAR_INIT(observer_default_invisibility, INVISIBILITY_OBSERVER)
 
 	/// The POI we're orbiting (orbit menu)
 	var/orbiting_ref
+
+	var/static/list/observer_hud_traits = list(
+		TRAIT_SECURITY_HUD,
+		TRAIT_MEDICAL_HUD,
+		TRAIT_DIAGNOSTIC_HUD,
+		TRAIT_BOT_PATH_HUD
+	)
 
 /mob/dead/observer/Initialize(mapload)
 	set_invisibility(GLOB.observer_default_invisibility)
@@ -149,7 +155,6 @@ GLOBAL_VAR_INIT(observer_default_invisibility, INVISIBILITY_OBSERVER)
 
 	SSpoints_of_interest.make_point_of_interest(src)
 	ADD_TRAIT(src, TRAIT_HEAR_THROUGH_DARKNESS, ref(src))
-	ADD_TRAIT(src, TRAIT_SECURITY_HUD, ref(src))
 
 /mob/dead/observer/get_photo_description(obj/item/camera/camera)
 	if(!invisibility || camera.see_ghosts)
@@ -760,14 +765,10 @@ This is the proc mobs get to turn into a ghost. Forked from ghostize due to comp
 	return
 
 /mob/dead/observer/proc/show_data_huds()
-	for(var/hudtype in datahuds)
-		var/datum/atom_hud/data_hud = GLOB.huds[hudtype]
-		data_hud.show_to(src)
+	add_traits(observer_hud_traits, REF(src))
 
 /mob/dead/observer/proc/remove_data_huds()
-	for(var/hudtype in datahuds)
-		var/datum/atom_hud/data_hud = GLOB.huds[hudtype]
-		data_hud.hide_from(src)
+	remove_traits(observer_hud_traits, REF(src))
 
 /mob/dead/observer/verb/toggle_data_huds()
 	set name = "Toggle Sec/Med/Diag HUD"

code/__DEFINES/atom_hud.dm
@@ -58,8 +58,8 @@
 #define DATA_HUD_SECURITY_ADVANCED 2
 #define DATA_HUD_MEDICAL_BASIC 3
 #define DATA_HUD_MEDICAL_ADVANCED 4
-#define DATA_HUD_DIAGNOSTIC_BASIC 5
-#define DATA_HUD_DIAGNOSTIC_ADVANCED 6
+#define DATA_HUD_DIAGNOSTIC 5
+#define DATA_HUD_BOT_PATH 6
 #define DATA_HUD_ABDUCTOR 7
 #define DATA_HUD_SENTIENT_DISEASE 8
 #define DATA_HUD_AI_DETECT 9

code/__DEFINES/traits/declarations.dm
@@ -336,6 +336,7 @@ Remember to update _globalvars/traits.dm if you're adding/removing/renaming trai
 #define TRAIT_SECURITY_HUD "sec_hud"
 /// for something granting you a diagnostic hud
 #define TRAIT_DIAGNOSTIC_HUD "diag_hud"
+#define TRAIT_BOT_PATH_HUD "bot_path_hud"
 /// Is a medbot healing you
 #define TRAIT_MEDIBOTCOMINGTHROUGH "medbot"
 #define TRAIT_PASSTABLE "passtable"

code/_globalvars/traits/_traits.dm
@@ -188,6 +188,7 @@ GLOBAL_LIST_INIT(traits_by_type, list(
 		"TRAIT_DETECTIVES_TASTE" = TRAIT_DETECTIVES_TASTE,
 		"TRAIT_DETECT_STORM" = TRAIT_DETECT_STORM,
 		"TRAIT_DIAGNOSTIC_HUD" = TRAIT_DIAGNOSTIC_HUD,
+		"TRAIT_BOT_PATH_HUD" = TRAIT_BOT_PATH_HUD,
 		"TRAIT_DISCOORDINATED_TOOL_USER" = TRAIT_DISCOORDINATED_TOOL_USER,
 		"TRAIT_DISEASELIKE_SEVERITY_MEDIUM" = TRAIT_DISEASELIKE_SEVERITY_MEDIUM,
 		"TRAIT_DISFIGURED" = TRAIT_DISFIGURED,

code/_globalvars/traits/admin_tooling.dm
@@ -60,6 +60,7 @@ GLOBAL_LIST_INIT(admin_visible_traits, list(
 		"TRAIT_DEFIB_BLACKLISTED" = TRAIT_DEFIB_BLACKLISTED,
 		"TRAIT_DEPRESSION" = TRAIT_DEPRESSION,
 		"TRAIT_DIAGNOSTIC_HUD" = TRAIT_DIAGNOSTIC_HUD,
+		"TRAIT_BOT_PATH_HUD" = TRAIT_BOT_PATH_HUD,
 		"TRAIT_DISCOORDINATED_TOOL_USER" = TRAIT_DISCOORDINATED_TOOL_USER,
 		"TRAIT_DISFIGURED" = TRAIT_DISFIGURED,
 		"TRAIT_DISK_VERIFIER" = TRAIT_DISK_VERIFIER,

code/datums/elements/digitalcamo.dm
@@ -27,17 +27,15 @@
 
 /datum/element/digitalcamo/proc/HideFromAIHuds(mob/living/target)
 	for(var/mob/living/silicon/ai/AI in GLOB.ai_list)
-		var/datum/atom_hud/M = GLOB.huds[AI.med_hud]
-		M.hide_single_atomhud_from(AI,target)
-		var/datum/atom_hud/S = GLOB.huds[AI.sec_hud]
-		S.hide_single_atomhud_from(AI,target)
+		for (var/hud_type in AI.silicon_huds)
+			var/datum/atom_hud/silicon_hud = GLOB.huds[hud_type]
+			silicon_hud.hide_single_atomhud_from(AI,target)
 
 /datum/element/digitalcamo/proc/UnhideFromAIHuds(mob/living/target)
 	for(var/mob/living/silicon/ai/AI in GLOB.ai_list)
-		var/datum/atom_hud/M = GLOB.huds[AI.med_hud]
-		M.unhide_single_atomhud_from(AI,target)
-		var/datum/atom_hud/S = GLOB.huds[AI.sec_hud]
-		S.unhide_single_atomhud_from(AI,target)
+		for (var/hud_type in AI.silicon_huds)
+			var/datum/atom_hud/silicon_hud = GLOB.huds[hud_type]
+			silicon_hud.unhide_single_atomhud_from(AI,target)
 
 /datum/element/digitalcamo/proc/on_examine(datum/source, mob/M)
 	SIGNAL_HANDLER

code/datums/hud.dm
@@ -8,17 +8,24 @@ GLOBAL_LIST_EMPTY(huds_by_category)
 
 //GLOBAL HUD LIST
 GLOBAL_LIST_INIT(huds, list(
-	DATA_HUD_SECURITY_BASIC = new/datum/atom_hud/data/human/security/basic(),
-	DATA_HUD_SECURITY_ADVANCED = new/datum/atom_hud/data/human/security/advanced(),
-	DATA_HUD_MEDICAL_BASIC = new/datum/atom_hud/data/human/medical/basic(),
-	DATA_HUD_MEDICAL_ADVANCED = new/datum/atom_hud/data/human/medical/advanced(),
-	DATA_HUD_DIAGNOSTIC_BASIC = new/datum/atom_hud/data/diagnostic/basic(),
-	DATA_HUD_DIAGNOSTIC_ADVANCED = new/datum/atom_hud/data/diagnostic/advanced(),
-	DATA_HUD_ABDUCTOR = new/datum/atom_hud/abductor(),
-	DATA_HUD_SENTIENT_DISEASE = new/datum/atom_hud/sentient_disease(),
-	DATA_HUD_AI_DETECT = new/datum/atom_hud/ai_detector(),
-	DATA_HUD_FAN = new/datum/atom_hud/data/human/fan_hud(),
-	DATA_HUD_MALF_APC = new/datum/atom_hud/data/malf_apc(),
+	DATA_HUD_SECURITY_BASIC = new /datum/atom_hud/data/human/security/basic(),
+	DATA_HUD_SECURITY_ADVANCED = new /datum/atom_hud/data/human/security/advanced(),
+	DATA_HUD_MEDICAL_BASIC = new /datum/atom_hud/data/human/medical/basic(),
+	DATA_HUD_MEDICAL_ADVANCED = new /datum/atom_hud/data/human/medical/advanced(),
+	DATA_HUD_DIAGNOSTIC = new /datum/atom_hud/data/diagnostic(),
+	DATA_HUD_BOT_PATH = new /datum/atom_hud/data/bot_path(),
+	DATA_HUD_ABDUCTOR = new /datum/atom_hud/abductor(),
+	DATA_HUD_SENTIENT_DISEASE = new /datum/atom_hud/sentient_disease(),
+	DATA_HUD_AI_DETECT = new /datum/atom_hud/ai_detector(),
+	DATA_HUD_FAN = new /datum/atom_hud/data/human/fan_hud(),
+	DATA_HUD_MALF_APC = new /datum/atom_hud/data/malf_apc(),
+))
+
+GLOBAL_LIST_INIT(trait_to_hud, list(
+	TRAIT_SECURITY_HUD = DATA_HUD_SECURITY_ADVANCED,
+	TRAIT_MEDICAL_HUD = DATA_HUD_MEDICAL_ADVANCED,
+	TRAIT_DIAGNOSTIC_HUD = DATA_HUD_DIAGNOSTIC,
+	TRAIT_BOT_PATH_HUD = DATA_HUD_BOT_PATH
 ))
 
 /datum/atom_hud

code/game/data_huds.dm
@@ -53,17 +53,13 @@
 	hud_icons = list(FAN_HUD)
 
 /datum/atom_hud/data/diagnostic
-
-/datum/atom_hud/data/diagnostic/basic
 	hud_icons = list(DIAG_HUD, DIAG_STAT_HUD, DIAG_BATT_HUD, DIAG_MECH_HUD, DIAG_BOT_HUD, DIAG_TRACK_HUD, DIAG_CAMERA_HUD, DIAG_AIRLOCK_HUD, DIAG_LAUNCHPAD_HUD)
 
-/datum/atom_hud/data/diagnostic/advanced
-	hud_icons = list(DIAG_HUD, DIAG_STAT_HUD, DIAG_BATT_HUD, DIAG_MECH_HUD, DIAG_BOT_HUD, DIAG_TRACK_HUD, DIAG_CAMERA_HUD, DIAG_AIRLOCK_HUD, DIAG_LAUNCHPAD_HUD, DIAG_PATH_HUD)
-
 /datum/atom_hud/data/bot_path
-	// This hud exists so the bot can see itself, that's all
-	uses_global_hud_category = FALSE
 	hud_icons = list(DIAG_PATH_HUD)
+
+/datum/atom_hud/data/bot_path/private
+	uses_global_hud_category = FALSE
 
 /datum/atom_hud/abductor
 	hud_icons = list(GLAND_HUD)

HolyTigerGod/Voidcrew

commit b8209f5a9e66dae87be814745a9440c2835d6405

fixes ghost huds (#85164)
## About The Pull Request
Fixes https://github.com/tgstation/tgstation/issues/85120 
I think it was introduced by
https://github.com/tgstation/tgstation/pull/84984
## Why It's Good For The Game
fix good
## Changelog
:cl:

fix: fixed ghost huds

/:cl:

code/modules/mob/dead/observer/observer.dm
@@ -150,6 +150,7 @@ GLOBAL_VAR_INIT(observer_default_invisibility, INVISIBILITY_OBSERVER)
 	. = ..()
 
 	grant_all_languages()
+	setup_hud_traits()
 	show_data_huds()
 	data_huds_on = 1
 