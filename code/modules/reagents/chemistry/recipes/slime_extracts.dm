HolyTigerGod/Voidcrew

commit f4a5c0a87e55d82b3cf7e985249ccdb97ec9086a

Foodening (#77887)

code/modules/reagents/chemistry/recipes/slime_extracts.dm
@@ -383,8 +383,8 @@
 		explosion(holder.my_atom, devastation_range = 1, heavy_impact_range = 3, light_impact_range = 6, explosion_cause = src)
 
 
-/datum/chemical_reaction/slime/slimecornoil
-	results = list(/datum/reagent/consumable/cornoil = 10)
+/datum/chemical_reaction/slime/slimeoil
+	results = list(/datum/reagent/consumable/nutriment/fat/oil = 10)
 	required_reagents = list(/datum/reagent/blood = 1)
 	required_container = /obj/item/slime_extract/oil
 

commit ea8cb6b87e0a88b795cd73fa6d8f57359f414622

Fix oil slime making vegetable oil instead of corn oil (#87730)
## About The Pull Request
Fixes bug caused by
https://github.com/tgstation/tgstation/commit/f4a5c0a87e55d82b3cf7e985249ccdb97ec9086a#diff-aedb81aab354c31e90007bd0ad4e8babf347ac7d6003c3b01da14021ff5a10f4R387

The wiki and code prior both say oil slimes should make corn oil, but
they currently make vegetable oil. Since corn oil is needed for
nitroglycerin and oil slimes are explosive-centric, it makes sense they
should be making corn oil.
## Why It's Good For The Game
Fixes: #81036
## Changelog
:cl:
fix: Fixes oil slimes making vegetable oil instead of corn oil
/:cl:

code/modules/reagents/chemistry/recipes/slime_extracts.dm
@@ -384,7 +384,7 @@
 
 
 /datum/chemical_reaction/slime/slimeoil
-	results = list(/datum/reagent/consumable/nutriment/fat/oil = 10)
+	results = list(/datum/reagent/consumable/nutriment/fat/oil/corn = 10)
 	required_reagents = list(/datum/reagent/blood = 1)
 	required_container = /obj/item/slime_extract/oil
 