HolyTigerGod/Voidcrew

commit 747311c092a874a046d7f534fc1f26bab1f71b92

Fixes `Robotize()` and player panel Make AI verb not putting dead player ghosts in the new mob (#87597)
## About The Pull Request

observer ghosts with no minds skip over mind transfer and get put in the
mobs via key transfer, while a dead player ghost has a copy of their
character's mind but with `active = FALSE`, which fails the transfer.
it's kinda weird how the player panel uses `AIize` for the AI but uses a
completely different mob transform for the borg. hmm

## Why It's Good For The Game

fixes #68643

## Changelog

:cl:
fix: fixed Make Cyborg admin verb and player panel Make AI not putting
the ghosted player in control if they weren't an observer
/:cl:

code/modules/mob/living/silicon/ai/ai.dm
@@ -120,7 +120,7 @@
 
 	create_eye()
 
-	if(target_ai.mind)
+	if(target_ai.mind && target_ai.mind.active)
 		target_ai.mind.transfer_to(src)
 		if(mind.special_role)
 			to_chat(src, span_userdanger("You have been installed as an AI! "))

code/modules/mob/transform_procs.dm
@@ -153,7 +153,7 @@
 	if(mind) //TODO //TODO WHAT
 		if(!transfer_after)
 			mind.active = FALSE
-		mind.transfer_to(new_borg)
+		mind.transfer_to(new_borg, TRUE)
 	else if(transfer_after)
 		new_borg.key = key
 

commit 495966eddfb92e7de26880d5ee75aa0c78776658

Fixes borg to ai connection (#87773)
## About The Pull Request
fixes #87651 which was caused by #87597
## Why It's Good For The Game
it is
## Changelog
:cl:

fix: fixed roundstart borgs not being synced to ai

/:cl:

code/modules/mob/living/silicon/ai/ai.dm
@@ -120,7 +120,7 @@
 
 	create_eye()
 
-	if(target_ai.mind && target_ai.mind.active)
+	if((target_ai.mind && target_ai.mind.active) || SSticker.current_state == GAME_STATE_SETTING_UP)
 		target_ai.mind.transfer_to(src)
 		if(mind.special_role)
 			to_chat(src, span_userdanger("You have been installed as an AI! "))